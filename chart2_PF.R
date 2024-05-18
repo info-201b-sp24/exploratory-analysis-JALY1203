029bc78236b70bf7e9b408cb3d2a05d52a87859e
JALY1203 <jaly120304@gmail.com>
2024-05-17 23:12:12 -0700

    finished chart 2

--- chart2_PF.R
+++ chart2_PF.R
@@ -1,5 +1,19 @@
 library("tidyverse")
 library("knitr")
 
-# read CSVs
+# read csv from file
 team_total_stats <- read.csv("Team Totals.csv")
+
+# Personal Fouls Per Season
+pf_total <- team_total_stats %>%
+  group_by(season) %>%
+  summarise(pf = sum(pf, na.rm = TRUE))
+
+# create graph
+ggplot(pf_total, aes(x = season, y = pf)) +
+  geom_line(color = "blue") +
+  geom_point(color = "red") +
+  labs(title = "Personal Fouls Per Season",
+       x = "NBA Season",
+       y = "Total Personal Fouls") +
+  theme_minimal()
