3553ef3b5f189b33059d66dc9727a39cbe872661
JALY1203 <jaly120304@gmail.com>
2024-05-17 17:23:54 -0700

    completed summary

--- summary.R
+++ summary.R
@@ -15,7 +15,8 @@ most_wins <- team_summary_wl %>%
 # Team with he most wins in one season
 
 most_dominant_season <- team_summary_wl %>%
-  filter(w == max(w))
+  filter(w == max(w)) %>%
+  select(season, team, w, l)
 
 # Team with the most points scored overall
 
@@ -23,19 +24,21 @@ most_points <- team_total_stats %>%
   group_by(team) %>%
   filter(team != "League Average") %>%
   summarise(total_points = sum(pts, na.rm = TRUE)) %>%
-  filter(total_points == max(total_points))
+  filter(total_points == max(total_points)) 
   
 # Team with the most points scored in a season
 
 most_points_season <- team_total_stats %>%
   filter(team != "League Average") %>%
-  filter(pts == max(pts, na.rm = TRUE))
+  filter(pts == max(pts, na.rm = TRUE))%>%
+  select(season, team, pts)
 
 # Season with most personal fouls
 
 highest_pf <- team_total_stats %>%
   filter(team != "League Average") %>%
-  filter(pf == max(pf, na.rm = TRUE))
+  filter(pf == max(pf, na.rm = TRUE)) %>%
+  select(season, team, pf)
 
 
 
