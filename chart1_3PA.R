692b530b567d5abc80a2189d01a6ead4d91cbf00
JALY1203 <jaly120304@gmail.com>
2024-05-17 21:57:10 -0700

    finished 1st chart

--- chart1_3PA.R
+++ chart1_3PA.R
@@ -1,7 +1,20 @@
-x_values <- seq(1, 3)
-y_values <- seq(1,3)
+library("tidyverse")
+library("knitr")
 
-library(ggplot2)
-ggplot() +
-  geom_point(aes(x=x_values, y = y_values))
+# read csv from file
+team_total_stats <- read.csv("Team Totals.csv")
+
+# 3 point total by season
+x3pa_total <- team_total_stats %>%
+  group_by(season) %>%
+  summarise(x3pa = sum(x3pa, na.rm = TRUE))
+
+# create graph
+ggplot(x3pa_total, aes(x = season, y = x3pa)) +
+  geom_line(color = "blue") +
+  geom_point(color = "red") +
+  labs(title = "3 Point Attempts Per Season",
+       x = "NBA Season",
+       y = "Total 3 Point Attempts") +
+  theme_minimal()
 
