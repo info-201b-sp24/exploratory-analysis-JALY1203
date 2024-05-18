6ec70d222f59f07364a29840c3b5dcdfe0c29a3b
JALY1203 <jaly120304@gmail.com>
2024-05-17 21:57:41 -0700

    started on chart 2 and 3

---
+++ chart2_PF.R
@@ -0,0 +1,5 @@
+library("tidyverse")
+library("knitr")
+
+# read CSVs
+team_total_stats <- read.csv("Team Totals.csv")
---
+++ chart3_WL.R
@@ -0,0 +1,5 @@
+library("tidyverse")
+library("knitr")
+
+team_summary_wl <- read.csv("Team Summaries.csv")
+team_total_stats <- read.csv("Team Totals.csv")
